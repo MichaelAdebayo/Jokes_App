858ebe37948f21e55bdbf2ecef764227d4d8f73f
Michael Adebayo <mikea14071@gmail.com>
2024-09-25 17:06:34 -0400

    Created Databases and additional pages, and made app functional

--- JokesApp/obj/Debug/net8.0/JokesApp.AssemblyInfo.cs
+++ JokesApp/obj/Debug/net8.0/JokesApp.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JokesApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0aab543eab43cda0d587c3c908712f575a32ada")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79fe15b5116761004f9a12ca3b0113f3b13482e")]
 [assembly: System.Reflection.AssemblyProductAttribute("JokesApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JokesApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
